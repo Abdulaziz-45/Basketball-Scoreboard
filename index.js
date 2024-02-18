b189896822f5467ea1a60cab1d617b7ef5eb2f9b
Abdulaziz-45 <159722053+Abdulaziz-45@users.noreply.github.com>
2024-02-17 19:33:51 -0500

    Add files to commit

---
+++ index.js
@@ -0,0 +1,63 @@
+let sumEl= document.getElementById("score-1")
+let totalEl= document.getElementById("score-2")
+let score= 0
+function add(){ 
+    score= score + 1
+    console.log(sumEl)
+    sumEl.innerText= score
+  
+ }
+    
+
+
+  
+  function add2(){ 
+    score= score + 2
+    sumEl.innerText= score
+    
+   } 
+    
+
+
+
+
+function add3(){ 
+    score= score + 3
+    sumEl.innerText= score
+   
+    
+    
+ }
+
+
+let score1= 0
+  
+function add4(){ 
+    score1= score1 + 1
+    totalEl.innerText= score1
+  
+ }
+    
+  
+
+  
+  function add5(){ 
+    score1= score1 + 2
+    totalEl.innerText= score1
+    
+   } 
+    
+
+
+
+
+function add6(){ 
+    score1= score1 + 3
+    console.log(totalEl)
+    totalEl.innerText= score1
+   
+    
+    
+ }
+
+
